9f019af37d99457309477611c2b032b253b36a86
kth701 <kth701@nate.com>
2018-12-21 13:39:53 +0900

    Project Class: 4. add line hotfix commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello World");
         System.out.println("project Diva: 2. hotfix commit");
+        System.out.println("Project Classic: 4. add line hotfix commit");
     }
 }
