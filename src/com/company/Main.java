7db4a0c5c5ab55ef992bf490df8b8c7350c5cf31
kth701 <kth701@nate.com>
2018-12-21 13:29:25 +0900

    Song for our world : 2. hofix add line commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
+        System.out.println("project Diva: 2. hotfix commit");
     }
 }
